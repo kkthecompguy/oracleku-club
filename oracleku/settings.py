2ca631d09c6ca4cad7989d2c32d9929719c96489
Kosam Omollo <hnkhosamomollo3@gmail.com>
2020-08-01 13:55:12 +0300

    allowed hosts

--- oracleku/settings.py
+++ oracleku/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_1$!@1)g%ws4tso)#=f-+5-bq%i_#cqke2s!7mljl2#flrp8^s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'oracleku-club.herokuapp.com']
 
 
 # Application definition
